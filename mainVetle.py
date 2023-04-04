946795ad02c723d47c40227726052a2de13375b1
vetlenilsenn <vetle.nilsen@live.no>
2023-04-04 16:26:58 +0200

    gitignore

--- mainVetle.py
+++ mainVetle.py
@@ -3,7 +3,11 @@ import numpy as np # linear algebra
 import seaborn as sns # visualize
 import matplotlib.pyplot as plt
 
-df = pd.read_csv(r'archive\student-mat.csv')
+df = pd.read_csv('archive\student-mat.csv')
+df.drop(["address"], ["famsize"])
+
+
+
 print(df)
 
 print(df['Dalc'])
